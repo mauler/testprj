a83557739201461edc4ee2804d77e2ced7e30a3a
Paulo R <proberto.macedo@gmail.com>
2018-04-04 02:05:47 -0300

    Added site list view

--- tmw/sites/views.py
+++ tmw/sites/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from tmw.sites.models import Site
+
+
+class SiteList(ListView):
+    model = Site
