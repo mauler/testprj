7d67395f5067f902a2557a7e4c4a9a7f66b23fc5
Paulo R <proberto.macedo@gmail.com>
2018-04-04 03:28:17 -0300

    Added summary methods to Site models
    Added tests for summary methods

--- tmw/sites/models.py
+++ tmw/sites/models.py
@@ -9,6 +9,56 @@ class Site(models.Model):
     objects = SiteManager()
     name = models.CharField(max_length=20)
 
+    def get_value_a_avg(self):
+        """ Returns the average of A values from the site
+
+        :returns: Decimal
+        """
+        avalues = []
+        for valuesentry in self.valuesentry_set.all():
+            avalues.append(valuesentry.value_a)
+
+        # Check for zero division error
+        if not avalues:
+            return 0
+        else:
+            return sum(avalues) / len(avalues)
+
+    def get_value_b_avg(self):
+        """ Returns the average of B values from the site
+
+        :returns: Decimal
+        """
+        bvalues = []
+        for valuesentry in self.valuesentry_set.all():
+            bvalues.append(valuesentry.value_b)
+
+        # Check for zero division error
+        if not bvalues:
+            return 0
+        else:
+            return sum(bvalues) / len(bvalues)
+
+    def get_value_a_sum(self):
+        """ Returns the sum of A values from the site
+
+        :returns: Decimal
+        """
+        avalues = []
+        for valuesentry in self.valuesentry_set.all():
+            avalues.append(valuesentry.value_a)
+        return sum(avalues)
+
+    def get_value_b_sum(self):
+        """ Returns the sum of B values from the site
+
+        :returns: Decimal
+        """
+        bvalues = []
+        for valuesentry in self.valuesentry_set.all():
+            bvalues.append(valuesentry.value_b)
+        return sum(bvalues)
+
 
 class ValuesEntry(models.Model):
     """ Holds A and B values entry for a Site for a specific date.
--- tmw/sites/tests/test_models.py
+++ tmw/sites/tests/test_models.py
@@ -7,7 +7,52 @@ from django.test import TestCase
 from tmw.sites.models import Site
 
 
-class ModelsTestCase(TestCase):
+class SiteTestCase(TestCase):
+
+    def setUp(self):
+        # insert sample data into the database
+        self.demo_site = Site.objects.create(name='Demo Site')
+
+        self.demo_site.valuesentry_set.create(entry_date=date(2012, 12, 1),
+                                              value_a=1,
+                                              value_b=2)
+
+        self.demo_site.valuesentry_set.create(entry_date=date(2012, 12, 2),
+                                              value_a=1,
+                                              value_b=2)
+
+        self.demo_site.valuesentry_set.create(entry_date=date(2012, 12, 3),
+                                              value_a=1,
+                                              value_b=5)
+
+        self.abc_site = Site.objects.create(name='ABC Site')
+
+        self.xyz_site = Site.objects.create(name='XYZ Site')
+
+    def test_summary_for_site_without_values_entries(self):
+        """ Check the values for sites with no entries. """
+
+        # Checks value A and B sum
+        self.assertEqual(self.abc_site.get_value_a_sum(), 0)
+        self.assertEqual(self.abc_site.get_value_b_sum(), 0)
+
+        # Checks value A and B average
+        self.assertEqual(self.abc_site.get_value_a_avg(), 0)
+        self.assertEqual(self.abc_site.get_value_b_avg(), 0)
+
+    def test_summary_for_site_with_entries(self):
+        """ Aggregate values a and b and returns it sums and average. """
+
+        # Checks value A and B sum
+        self.assertEqual(self.demo_site.get_value_a_sum(), 3)
+        self.assertEqual(self.demo_site.get_value_b_sum(), 9)
+
+        # Checks value A and B average
+        self.assertEqual(self.demo_site.get_value_a_avg(), 1)
+        self.assertEqual(self.demo_site.get_value_b_avg(), 3)
+
+
+class DatabaseIntegrityTestCase(TestCase):
 
     def setUp(self):
         self.demo_site = Site.objects.create(name='Demo Site')
