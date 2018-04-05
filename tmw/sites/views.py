213642137d0c6cbe05530eb2a92e22d5c930fb83
Paulo R <proberto.macedo@gmail.com>
2018-04-04 22:21:27 -0300

    Removed unused import

--- tmw/sites/views.py
+++ tmw/sites/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
-from tmw.sites.models import Site, ValuesEntry
+from tmw.sites.models import Site
 
 
 class SiteList(ListView):
